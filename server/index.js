e289f0cd886444788b01b91aa712ebe6e53ca9cf
Kibuto <nguyentrongnhanc8@gmail.com>
2020-02-19 10:18:52 +0700

    categories ( server )

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
+const port = process.env.PORT;
+const bodyParser = require('body-parser');
+
+const indexRoutesProduct = require('./routes/api/products');
+const createRoutesProduct = require('./routes/api/products');
+const indexRoutesCategory = require('./routes/api/categories');
+const signInRoutes = require('./routes/api/auth');
+const signUpRoutes = require('./routes/api/auth');
+const verifyRoutes = require('./routes/api/auth');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', indexRoutesProduct);
+app.use('/api', createRoutesProduct);
+app.use('/api', indexRoutesCategory);
+app.use('/api', signInRoutes);
+app.use('/api', signUpRoutes);
+app.use('/api', verifyRoutes);
+
+
+app.listen(port, () => {
+    console.log(`App running port ${port}`);
+});
