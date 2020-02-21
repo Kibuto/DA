4a96e1770436fe7db6cedb9944cccff9b5b1716f
Kibuto <nguyentrongnhanc8@gmail.com>
2020-02-21 09:08:09 +0700

    design categories screen

--- client/Components/CategoryListItem.js
+++ client/Components/CategoryListItem.js
@@ -1,36 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+export default class CategoryListItem extends Component {
 
-export default function CategoryListItem(props) {
-    const { category, onPress } = props;
-    return (
-        <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
-            <View style={styles.container}>
-                <Text style={styles.title}>{category.name}</Text>
-                <Image style={styles.categoryImg} source={{ uri: category.img }} />
-            </View>
-        </TouchableOpacity>
-    )
+    _handleRandomColor = () => {
+        const { random } = this.props;
+        let index = Math.round(random()*10);
+        if(index < 3) {
+            return '#4C7C9B';
+        }
+        else if(index < 7) {
+            return '#B9314F';
+        }
+        else {
+            return '#F5853F';
+        }
+    }
+
+    render() {
+        const { category } = this.props;
+        return (
+            <TouchableOpacity style={[styles.container, {backgroundColor: this._handleRandomColor()}]} activeOpacity={0.5}>
+                <Text style={styles.header}>{category.name}</Text>
+                <View style={{elevation: 1, position: 'relative'}}>
+                    <View style={styles.boxShadow} />
+                    <Image resizeMode='stretch' style={styles.categoryImg} source={{ uri: category.img }} />
+                </View>
+            </TouchableOpacity>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
-        padding: 16,
-        borderRadius: 4,
-        borderColor: '#CCC',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        backgroundColor: '#FFF',
+        paddingVertical: 16,
+        paddingHorizontal: 32,
+        borderRadius: 10,
         marginBottom: 16
     },
-    title: {
-        textTransform: 'uppercase',
-        marginBottom: 8,
-        fontWeight: '700'
+    header: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: '700',
+        marginBottom: 15,
+        color: '#FFF'
     },
     categoryImg: {
-        width: 64,
-        height: 64
+        height: 150,
+        marginBottom: 10,
+        borderRadius: 8,
+    },
+    boxShadow: {
+        backgroundColor: '#000', 
+        position: "absolute", 
+        top: 5, 
+        left: 8, 
+        height: '95%', 
+        width: `100%`, 
+        borderRadius: 8, 
+        opacity: 0.3 
     }
 });
+// import React from 'react';
+// import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+// export default function CategoryListItem(props) {
+//     const { category, onPress } = props;
+//     return (
+//         <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+//             <View style={styles.container}>
+//                 <Text style={styles.title}>{category.name}</Text>
+//                 <Image style={styles.categoryImg} source={{ uri: category.img }} />
+//             </View>
+//         </TouchableOpacity>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         alignItems: 'center',
+//         padding: 16,
+//         borderRadius: 4,
+//         borderColor: '#CCC',
+//         borderStyle: 'solid',
+//         borderWidth: 2,
+//         backgroundColor: '#FFF',
+//         marginBottom: 16
+//     },
+//     title: {
+//         textTransform: 'uppercase',
+//         marginBottom: 8,
+//         fontWeight: '700'
+//     },
+//     categoryImg: {
+//         width: 64,
+//         height: 64
+//     }
+// });
--- client/Screen/Categories.js
+++ client/Screen/Categories.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { View, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
-import LogoTitle from '../Components/LogoTitle';
 import CategoryListItem from "../Components/CategoryListItem";
 import { HOST } from '../key';
 class Categories extends Component {
 
     static navigationOptions = {
-        headerTitle: () => <LogoTitle />,
         headerTitleAlign: {
             textAlign: 'center'
         }
@@ -15,7 +13,7 @@ class Categories extends Component {
     state = {
         categories: []
     }
-    
+
     componentDidMount() {
         this._handleCallApi();
     }
@@ -33,29 +31,29 @@ class Categories extends Component {
     }
 
     render() {
-        const { navigation } = this.props;
         const { categories } = this.state;
         return (
-            <>
-                {
-                    categories.length < 1 ? 
-                    <View style={[styles.container_indicator, styles.horizontal]}>
-                        <ActivityIndicator size="large" color="#0000ff" />
-                    </View> :
-                    <FlatList 
-                        data={categories}
-                        //
-                        renderItem={({ item }) => <CategoryListItem category={item} onPress={() => navigation.navigate('Products', { categoryProduct: item })}/>}
-                        keyExtractor={(item) => `${item.id}`}
-                        contentContainerStyle={{paddingHorizontal: 16, backgroundColor: '#fffaff', paddingTop: 16}}
-                    />
+            <FlatList 
+                data={categories}
+                numColumns={2}
+                renderItem={({ item }) =>
+                    <View style={{flex: 1, paddingHorizontal: 10}}>
+                        <CategoryListItem random={Math.random} category={item}/>
+                    </View>
                 }
-            </>
+                keyExtractor={(item) => `${item.id}`}
+                contentContainerStyle={styles.container}
+            />
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10, 
+        backgroundColor: '#fffaff',
+        paddingTop: 16
+    },
     container_indicator: {
         flex: 1,
         justifyContent: 'center',
@@ -69,3 +67,74 @@ const styles = StyleSheet.create({
 });
 
 export default Categories;
+// import React, { Component } from 'react';
+// import { View, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+// import LogoTitle from '../Components/LogoTitle';
+// import CategoryListItem from "../Components/CategoryListItem";
+// import { HOST } from '../key';
+// class Categories extends Component {
+
+//     static navigationOptions = {
+//         headerTitle: () => <LogoTitle />,
+//         headerTitleAlign: {
+//             textAlign: 'center'
+//         }
+//     };
+
+//     state = {
+//         categories: []
+//     }
+    
+//     componentDidMount() {
+//         this._handleCallApi();
+//     }
+
+//     _handleCallApi = () => {
+//         fetch(`${HOST}/api/categories`)
+//             .then(res => res.json())
+//             .then(json => {
+//                 if(json.success) {
+//                     this.setState({
+//                         categories: json.message
+//                     })
+//                 }
+//         });
+//     }
+
+//     render() {
+//         const { navigation } = this.props;
+//         const { categories } = this.state;
+//         return (
+//             <>
+//                 {
+//                     categories.length < 1 ? 
+//                     <View style={[styles.container_indicator, styles.horizontal]}>
+//                         <ActivityIndicator size="large" color="#0000ff" />
+//                     </View> :
+//                     <FlatList 
+//                         data={categories}
+//                         //
+//                         renderItem={({ item }) => <CategoryListItem category={item} onPress={() => navigation.navigate('Products', { categoryProduct: item })}/>}
+//                         keyExtractor={(item) => `${item.id}`}
+//                         contentContainerStyle={{paddingHorizontal: 16, backgroundColor: '#fffaff', paddingTop: 16}}
+//                     />
+//                 }
+//             </>
+//         )
+//     }
+// }
+
+// const styles = StyleSheet.create({
+//     container_indicator: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//     },
+//     horizontal: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-around',
+//         padding: 10
+//     }
+// });
+
+// export default Categories;
