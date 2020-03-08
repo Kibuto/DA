f199eeecdbbb819e7d5a906021c6d1fe2b6e694b
Kibuto <nguyentrongnhanc8@gmail.com>
2020-03-08 14:38:42 +0700

    check product (client)

---
+++ client/Components/CheckProductItem.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Button } from "native-base";
+export default class CheckProduct extends Component {
+    render() {
+        const { product } = this.props;
+        console.log(product);
+        return (
+            <TouchableOpacity style={styles.container} activeOpacity={0.9}>
+                <View style={styles.content_left}>
+                    <Image resizeMode='stretch' style={styles.productImg} source={{ uri: product.images[0].url }} />
+                    <View style={styles.boxShadow} />
+                </View>
+                <View style={styles.content_right}>
+                    <Text numberOfLines={1} ellipsizeMode='tail' style={styles.name}>{product.name}</Text>
+                    {/* <Text style={{ marginBottom: 10}}>by <Text style={styles.author}>{product.author}</Text></Text> */}
+                    <Text style={styles.description} numberOfLines={3} ellipsizeMode='tail'>{product.description}</Text>
+                    <Text style={{alignSelf: 'flex-end', color: '#666', marginBottom: 10}}>Seller <Text style={{ fontWeight: '700', fontSize: 18, fontStyle: 'italic', color: '#666', textTransform: 'capitalize'}}>{product.seller}</Text></Text>
+                    <Button block rounded disabled success={product.isCheck}>
+                        <Text style={{color: '#FFF', fontSize: 16}}>Pending Censorship</Text>
+                    </Button>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        flexDirection: 'row', 
+        marginBottom: 30, 
+        backgroundColor: '#D9DFE3', 
+        paddingVertical: 15, 
+        borderRadius: 10, 
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: '#DDD'
+    },
+    productImg: {
+        height: 180,
+        borderRadius: 10,
+        zIndex: 999
+    },
+    header: {
+        fontSize: 24, 
+        fontWeight: '700', 
+        marginBottom: 5
+    },
+    content_left: {
+        flex: 2/5, 
+        position: 'relative', 
+        justifyContent:'center'
+    },
+    boxShadow: {
+        backgroundColor: '#000', 
+        opacity: .5, 
+        position: 'absolute', 
+        height: `100%`, 
+        width: `100%`, 
+        bottom: -5, 
+        left: 6, 
+        borderRadius: 5
+    },
+    content_right: {
+        flex: 3/5, 
+        paddingVertical: 15, 
+        marginLeft: 15
+    },
+    name: {
+        fontWeight:'700', 
+        fontSize: 24,
+        marginBottom: 10,
+        color: '#D90368'
+    },
+    author: {
+        color: '#888', 
+        fontSize: 15
+    },
+    description: {
+        letterSpacing: 1, 
+        fontSize: 16, 
+        marginBottom: 10
+    },
+    star: {
+        flexDirection: 'row', 
+        marginBottom: 5, 
+        alignItems: 'center'
+    }
+})
---
+++ client/Screen/CheckProduct.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { Container, Header, Title, Body, Content } from "native-base";
+import { ColorBg, ColorHeader, HOST } from '../key';
+import CheckProductItem from '../Components/CheckProductItem';
+export default class CheckProduct extends Component {
+
+    state = {
+        products: [],
+        msg: ''
+    }
+
+    componentDidMount() {
+        this._handleCallApi();
+    }
+
+    _handleCallApi = () => {
+        const { token } = this.props.route.params;
+        const bearer = `Bearer ${token}`;
+        fetch(`${HOST}/api/getProduct`, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': bearer,
+                'Content-Type': 'application/json'
+            })
+        })
+        .then(res => res.json())
+        .then(json => {
+            if(json.success) {
+                this.setState({products: json.product});
+            } else {
+                this.setState({msg: json.message});
+            }
+        })
+    }
+
+    render() {
+        const { products, msg } = this.state;
+        return (
+            <Container style={{backgroundColor: ColorBg}}>
+                <Header style={{backgroundColor: ColorHeader}} androidStatusBarColor='#000' transparent>
+                    <Body>
+                        <Title style={{fontSize: 26, color: '#D90368', fontWeight:'700', alignSelf: 'center'}}>Info Product</Title>
+                    </Body>
+                </Header>
+                {
+                    msg.length === 0 ? 
+                    <FlatList
+                        data={products}
+                        renderItem={({ item }) => 
+                            <View style={styles.wrapper}>
+                                <CheckProductItem product={item} />
+                            </View>
+                        }
+                        keyExtractor={(item) => `${item._id}`}
+                        contentContainerStyle={styles.container}
+                    /> : 
+                    <View style={styles.err}>
+                        <Text style={styles.text}>{msg}</Text>
+                    </View>
+                }
+            </Container>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 10,
+        paddingTop: 20,
+        backgroundColor: ColorBg
+    },
+    err: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 22, 
+        fontStyle: 'italic', 
+        fontWeight: '700'
+    }
+})
