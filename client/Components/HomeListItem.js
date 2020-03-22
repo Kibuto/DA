686ff002c86a8fa0266e3af05a9ff52e313db0a2
Kibuto <nguyentrongnhanc8@gmail.com>
2020-03-22 22:25:29 +0700

    home screen (v1)

--- client/Components/HomeListItem.js
+++ client/Components/HomeListItem.js
@@ -1,15 +1,125 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { AirbnbRating } from 'react-native-elements';
 export default class Home extends Component {
     render() {
+        const { product, category } = this.props;
         return (
-            <View>
-                <Text>Home list item</Text>
-            </View>
+            <>
+                {
+                    category ? 
+                    <TouchableOpacity style={styles.container} activeOpacity={0.9}>
+                        <View style={styles.content_left}>
+                            <Image resizeMode='stretch' style={styles.productImg_true} source={{ uri: product.images[0].url }} />
+                            {/* <View style={styles.boxShadow} /> */}
+                        </View>
+                        <View style={styles.content_right}>
+                            <Text numberOfLines={1} ellipsizeMode='tail' style={styles.name}>{product.name}</Text>
+                            {/* <Text numberOfLines={1} ellipsizeMode='tail' style={{ marginBottom: 3}}>by <Text style={styles.author_true}>{product.author}</Text></Text> */}
+                            <Text style={styles.description} numberOfLines={3} ellipsizeMode='tail'>{product.description}</Text>
+                            {/* <Text style={{alignSelf: 'flex-end', color: '#666', marginBottom: 3}}>Seller <Text style={styles.seller}>{product.seller}</Text></Text> */}
+                            <View style={styles.star}>
+                                <AirbnbRating
+                                    count={5}
+                                    defaultRating={4}
+                                    size={20}
+                                    showRating={false}
+                                />
+                                <Text style={{ marginLeft: 15, fontSize: 18, fontWeight: '700' }}>4</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity> : 
+                    <TouchableOpacity activeOpacity={0.9}>
+                        <Image resizeMode='contain' style={styles.productImg_false} source={{ uri: product.images[0].url }} />
+                        <Text numberOfLines={1} ellipsizeMode='tail' style={styles.author_false}>{product.author}</Text>
+                    </TouchableOpacity>
+                }
+            </>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        flexDirection: 'row', 
+        backgroundColor: '#FFF', 
+        borderRadius: 10, 
+        paddingHorizontal: 10,
+        marginRight: 5,
+        borderWidth: 3,
+        borderColor: '#EEE',
+        width: 320,
+        paddingVertical: 10
+    },
+    productImg_true: {
+        height: 160,
+        borderWidth: 1,
+        borderColor: '#DDD'
+        //borderRadius: 10,
+    },
+    productImg_false: {
+        height: 160, 
+        width: 120, 
+        borderRadius: 10
+    },
+    header: {
+        fontSize: 24, 
+        fontWeight: '700', 
+        marginBottom: 3
+    },
+    content_left: {
+        flex: 2/5, 
+        position: 'relative', 
+        justifyContent:'center',
+        borderRadius: 10
+    },
+    boxShadow: {
+        backgroundColor: '#000', 
+        opacity: .5, 
+        position: 'absolute', 
+        height: `88%`, 
+        width: `100%`, 
+        bottom: 8, 
+        left: 5,
+        borderRadius: 8
+    },
+    content_right: {
+        flex: 3/5, 
+        paddingVertical: 15, 
+        marginLeft: 15
+    },
+    name: {
+        fontWeight:'700', 
+        fontSize: 24,
+        marginBottom: 3,
+        color: '#D90368'
+    },
+    author_true: {
+        color: '#888', 
+        fontSize: 15
+    },
+    author_false: {
+        marginVertical: 5, 
+        textAlign: 'center', 
+        width: 120, 
+        color: '#888'
+    },
+    description: {
+        letterSpacing: 1, 
+        fontSize: 16, 
+        marginBottom: 3
+    },
+    star: {
+        flexDirection: 'row', 
+        marginBottom: 3, 
+        alignItems: 'center'
+    },
+    seller: {
+        fontWeight: '700', 
+        fontSize: 18, 
+        fontStyle: 'italic', 
+        color: '#666', 
+        textTransform: 'capitalize'
+    }
 })
--- client/Screen/Home.js
+++ client/Screen/Home.js
@@ -1,45 +1,164 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ActivityIndicator, SafeAreaView, Text } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, Dimensions, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { Container, Header, Title, Content, Button, Icon, Left, Body, Right } from "native-base";
-import LogoTitle from '../Components/LogoTitle';
-import { ColorBg, ColorHeader } from '../key';
+import HomeListItem from '../Components/HomeListItem';
+import { ColorBg, ColorHeader, HOST } from '../key';
+const { height, width } = Dimensions.get('screen');
 export default class Home extends Component {
 
-    static navigationOptions = {
-        headerTitle: () => <LogoTitle />,
-        headerTitleAlign: {
-            textAlign: 'center'
-        }
-    };
+    state = {
+        data: [],
+        like: [],
+        categories: [
+            {
+                id: 1,
+                title: 'All',
+                selected: true,
+                category: 'Romance'
+            }, 
+            {
+                id: 2,
+                title: 'Recommended',
+                selected: false,
+                category: 'Mystery'
+            }, 
+            {
+                id: 3,
+                title: 'Popular books',
+                selected: false,
+                category: 'History'
+            },
+            {
+                id: 4,
+                title: 'Best seller',
+                selected: false,
+                category: 'Healthy'
+            }
+        ]
+    }
+
+    // componentDidMount() {
+    //     this._handleBestSeller();
+    //     this._handleRelease();
+    // }
+
+    // _handleBestSeller = () => {
+    //     const id = 'Romance'
+    //     fetch(`${HOST}/api/products?category=${id}`)
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             if(json.success) {
+    //                 this.setState({
+    //                     bestSeller: json.message
+    //                 })
+    //             }
+    //             else {
+    //                 console.log('Ko lay duoc products');
+    //             }
+    //     });
+    // }
+
+    // _handleRelease = () => {
+    //     const id = 'Mystery'
+    //     fetch(`${HOST}/api/products?category=${id}`)
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             if(json.success) {
+    //                 this.setState({
+    //                     release: json.message
+    //                 })
+    //             }
+    //             else {
+    //                 console.log('Ko lay duoc products');
+    //             }
+    //     });
+    // }
+    componentDidMount() {
+        this._handleOnSelected(0);
+        this._handleCallApi();
+    }
+
+    _handleCallApi = () => {
+        fetch(`${HOST}/api/products?category=Horror`)
+            .then(res => res.json())
+            .then(json => {
+                if(json.success) {
+                    this.setState({like: json.message})
+                }
+                else {
+                    console.log('Ko lay duoc products');
+                }
+        });
+    }
+
+    _handleOnSelected = (key) => {
+        this.state.categories.map((item, index) => {
+            if(key === index) {
+                fetch(`${HOST}/api/products?category=${item.category}`)
+                    .then(res => res.json())
+                    .then(json => {
+                        if(json.success) {
+                            this.setState({data: json.message})
+                        }
+                        else {
+                            console.log('Ko lay duoc products');
+                        }
+                });
+                item.selected = true;
+            } else {
+                item.selected = false
+            }
+        })
+        this.setState({categories: this.state.categories})
+    }
     
     render() {
+        const { data, categories, like } = this.state;
         return (
-            // <>{
-            //     categories.length < 1 ? 
-            //     <View style={[styles.container_indicator, styles.horizontal]}>
-            //         <ActivityIndicator size="large" color="#0000ff" />
-            //     </View> :
-            //     <FlatList 
-            //         data={categories}
-            //         numColumns={2}
-            //         renderItem={({ item }) =>
-            //             <View style={{flex: 1, paddingHorizontal: 10}}>
-            //                 <CategoryListItem random={Math.random} category={item} onPress={() => navigation.navigate('Products', { categoryProduct: item })}/>
-            //             </View>
-            //         }
-            //         keyExtractor={(item) => `${item.id}`}
-            //         contentContainerStyle={styles.container}
-            //     />
-            // }</>
             <Container style={{backgroundColor: ColorBg}}>
                 <Header style={{backgroundColor: ColorHeader}} androidStatusBarColor='#000' transparent>
                     <Body>
                         <Title style={{fontSize: 26, color: '#D90368', fontWeight:'700', alignSelf: 'center'}}>Home</Title>
                     </Body>
                 </Header>
-                <Content contentContainerStyle={styles.err}>
-                    <Text style={{color: '#888'}}>Home</Text>
-                </Content>
+                <Image resizeMode='stretch' style={{height: 150, borderRadius: 20, marginBottom: 15}} source={{uri: 'https://www.netguru.co/hubfs/Blog_posts_-_images/pexels-photo-46274.jpeg'}}/>
+                <View>
+                    <FlatList 
+                        data={ categories }
+                        renderItem={({ item, index, separators }) => 
+                            <TouchableOpacity onPress={() => this._handleOnSelected(index)} >
+                                <Text style={[item.selected&&{fontWeight: '700'}, {fontSize: 17, marginHorizontal: 7}]}>{item.title}</Text>
+                            </TouchableOpacity>
+                        }
+                        horizontal={true}
+                        keyExtractor={(item) => `${item.id}`}
+                        //extraData={selected}
+                        contentContainerStyle={{marginLeft: 5}}
+                    />
+
+                    <FlatList 
+                        data={ data }
+                        renderItem={({ item }) => 
+                            <HomeListItem category={true} product={item}/>
+                        }
+                        horizontal={true}
+                        keyExtractor={(item) => `${item._id}`}
+                        contentContainerStyle={styles.container}
+                        showsHorizontalScrollIndicator={false}
+                    />
+
+                    <Text style={{fontSize: 18, fontWeight: '700', marginLeft: 15, marginVertical: 10}}>You may also like</Text>
+                    <FlatList 
+                        data={ like }
+                        renderItem={({ item }) => 
+                            <HomeListItem category={false} product={item}/>
+                        }
+                        horizontal={true}
+                        keyExtractor={(item) => `${item._id}`}
+                        contentContainerStyle={{marginLeft: 10}}
+                        showsHorizontalScrollIndicator={false}
+                    />
+                </View>
             </Container>
         )
     }
@@ -47,9 +166,8 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        paddingTop: 15,
+        marginHorizontal: 10
     },
     container_indicator: {
         flex: 1,
@@ -65,5 +183,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    wrapper: {
+        marginHorizontal: 10
     }
 })
