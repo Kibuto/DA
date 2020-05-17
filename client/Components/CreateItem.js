c548bf01e43d6803c8737f889adeef01add1aaa7
Kibuto <nguyentrongnhanc8@gmail.com>
2020-05-17 15:18:16 +0700

    edit create screen

--- client/Components/CreateItem.js
+++ client/Components/CreateItem.js
@@ -4,8 +4,9 @@ import ActionSheet from 'react-native-actionsheet';
 import ImagePicker from 'react-native-image-picker';
 import { Input, Item, Label, Form, Icon, Textarea } from 'native-base';
 import Logo from '../images/Brand-white.png';
+import { validateMoney } from '../utils/Validation';
 import { HOST } from '../key';
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 class CreateItem extends Component {
 
     state = {
@@ -47,7 +48,7 @@ class CreateItem extends Component {
                         })
                     }
                 })
-            break;
+                break;
             case 1:
                 ImagePicker.launchImageLibrary(options, (response) => {
                     if (response.didCancel) {
@@ -64,9 +65,9 @@ class CreateItem extends Component {
                         })
                     }
                 })
-            break;
+                break;
             default:
-            break;
+                break;
         }
     };
 
@@ -92,15 +93,15 @@ class CreateItem extends Component {
             }),
             body: formData
         })
-        .then(res => res.json())
-        .then(json => {
-            console.log(json);
-            if(json.success) {
-                this.showAlert(json.message);
-            } else {
-                console.log('Error')
-            }
-        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                if (json.success) {
+                    this.showAlert(json.message);
+                } else {
+                    console.log('Error')
+                }
+            })
     };
 
     showAlert = (str) => {
@@ -109,63 +110,81 @@ class CreateItem extends Component {
             `${str}`,
             'You are going to be redirected to the Products Screen',
             [
-                {text: 'OK', onPress: () => navigation.navigate('Categories')},
+                { text: 'OK', onPress: () => navigation.navigate('Categories') },
             ]
         )
     }
 
+    validatePrice = (money) => {
+        const { price } = this.state;
+        if (validateMoney(money)) {
+            console.log('Money');
+            this.setState({
+                price,
+                errorPrice: false,
+                errorMessage: ''
+            })
+        } else {
+            this.setState({
+                errorPrice: true,
+                errorMessage: 'Price invalid (Must be at least 5 number)'
+            })
+        }
+    }
+
     render() {
-        const { name, price, errorPrice, errorName, image, category, description } = this.state;
+        const { name, price, errorPrice, errorName, image, category, description, errorMessage } = this.state;
         return (
-            <KeyboardAvoidingView behavior='height'>
+            <KeyboardAvoidingView>
                 <View style={styles.container}>
-                    <Image resizeMode='contain' source={Logo} style={styles.logo}/>
-                    <Text style={{textAlign: 'center', fontSize: 18, fontWeight: '700', marginBottom: 15}}>What would you like to sell?</Text>
-                    <Item floatingLabel style={{marginBottom: 20}}>
+                    <Image resizeMode='contain' source={Logo} style={styles.logo} />
+                    <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: '700', marginBottom: 15 }}>What would you like to sell?</Text>
+                    {errorMessage ? <Text style={{ color: '#E9446A', fontSize: 16, fontWeight: '700', marginVertical: 10 }}>{errorMessage}</Text> : null}
+                    <Item floatingLabel style={{ marginBottom: 20 }}>
                         <Label>Name Product</Label>
-                        <Input 
-                            autoCapitalize='none' 
+                        <Input
+                            autoCapitalize='none'
                             autoCorrect={false}
-                            onChangeText={name => this.setState({name})}
+                            onChangeText={name => this.setState({ name })}
                         />
                     </Item>
-                    <Item floatingLabel style={{marginBottom: 20}}>
+                    <Item floatingLabel style={{ marginBottom: 20 }} error={errorPrice.length ? true : false}>
                         <Label>Price product</Label>
-                        <Input 
-                            autoCapitalize='none' 
+                        <Input
+                            autoCapitalize='none'
                             autoCorrect={false}
-                            onChangeText={price => this.setState({price})}
+                            onChangeText={price => this.validatePrice(price)}
                         />
                     </Item>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginBottom: 10}}>
-                        <Text style={{flex: 2/5, fontSize: 18}}>Category: </Text>
-                        <Text style={{flex: 2/5, fontSize: 18}}>{category}</Text>
-                        <Picker 
-                            style={{borderColor: 'red', borderWidth: 1, height: 32, width: 32, flex: 1/5 }}
-                            selectedValue={category} 
-                            onValueChange={(itemValue, itemIndex) => this.setState({category: itemValue})}>
-                            <Picker.Item label = "History" value = "History" />
-                            <Picker.Item label = "Adventure" value = "Adventure" />
-                            <Picker.Item label = "Mystery" value = "Mystery" />
-                            <Picker.Item label = "Romance" value = "Romance" />
-                            <Picker.Item label = "Horror" value = "Horror" />
-                            <Picker.Item label = "Different..." value = "Different..." />
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginBottom: 10 }}>
+                        <Text style={{ flex: 2 / 5, fontSize: 18 }}>Category: </Text>
+                        <Text style={{ flex: 2 / 5, fontSize: 18 }}>{category}</Text>
+                        <Picker
+                            style={{ borderColor: 'red', borderWidth: 1, height: 32, width: 32, flex: 1 / 5 }}
+                            selectedValue={category}
+                            onValueChange={(itemValue, itemIndex) => this.setState({ category: itemValue })}>
+                            <Picker.Item label="History" value="History" />
+                            <Picker.Item label="Adventure" value="Adventure" />
+                            <Picker.Item label="Mystery" value="Mystery" />
+                            <Picker.Item label="Romance" value="Romance" />
+                            <Picker.Item label="Horror" value="Horror" />
+                            <Picker.Item label="Different..." value="Different..." />
                         </Picker>
                     </View>
-                    <Textarea onChangeText={description => this.setState({description})} rowSpan={3} style={{width: width - 30}} bordered placeholder="Description product" />
-                    
-                    
+                    <Textarea onChangeText={description => this.setState({ description })} rowSpan={3} style={{ width: (width / 100) * 85 }} bordered placeholder="Description product" />
+
+
                     <TouchableOpacity style={styles.btn} activeOpacity={0.5} onPress={this.showActionSheet}>
                         <Text style={styles.btn_text} >Choose Image</Text>
                     </TouchableOpacity>
-                    
-                    { image ? <Image resizeMode='contain' source={{ uri: image }} style={{ width: 120, height: 120, marginTop: 10 }} /> : null }
-                    
-                    { 
-                        image ? 
-                        <TouchableOpacity style={styles.btn} activeOpacity={0.5} onPress={this._handleOnSale}>
-                            <Text style={styles.btn_text} >Sell</Text>
-                        </TouchableOpacity> : null 
+
+                    {image ? <Image resizeMode='contain' source={{ uri: image }} style={{ width: 120, height: 120, marginTop: 10 }} /> : null}
+
+                    {
+                        image ?
+                            <TouchableOpacity style={styles.btn} activeOpacity={0.5} onPress={this._handleOnSale}>
+                                <Text style={styles.btn_text} >Sell</Text>
+                            </TouchableOpacity> : null
                     }
 
                     <ActionSheet
@@ -189,19 +208,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 16,
-        borderRadius: 4,
         backgroundColor: '#FFF',
-        height: `100%`
+        borderRadius: 10,
+        width: '92%',
+        alignSelf: 'center',
+        shadowRadius: 10,
+        shadowOpacity: .9,
+        shadowOffset: {
+            width: 0,
+            hieght: 2
+        },
+        elevation: 3,
+        marginBottom: 20
     },
     logo: {
         width: `100%`,
-        height: `15%`
+        height: 100
     },
     btn: {
         width: `50%`,
         backgroundColor: `#2089dc`,
         borderRadius: 5,
-        marginTop: 20
+        marginTop: 20,
+        marginBottom: 20
     },
     btn_text: {
         textAlign: 'center',
--- client/Screen/Create.js
+++ client/Screen/Create.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { Container, Header, Title, Body, Content } from "native-base";
 import CreateItem from '../Components/CreateItem';
-
+import { ColorBg, ColorHeader } from '../key';
+const { width, height } = Dimensions.get('window');
 class ScreenCreate extends Component {
 
     render() {
         const { token } = this.props.route.params;
         return (
-            <View style={styles.container}>
-                <CreateItem navigation={this.props.navigation} token={token} />
-            </View>
+            <Container style={{ backgroundColor: ColorBg }}>
+                <Header style={{ backgroundColor: ColorHeader }} androidStatusBarColor='#000' transparent>
+                    <Body>
+                        <Title style={styles.title_header}>Create</Title>
+                    </Body>
+                </Header>
+                <Content style={styles.container}>
+                    <CreateItem navigation={this.props.navigation} token={token} />
+                </Content>
+            </Container>
         )
     }
 }
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#FFF',
-        height: '100%'
+        //backgroundColor: '#FFF',
+        marginTop: 20,
+        height: height
+    },
+    title_header: {
+        fontSize: 26,
+        color: '#D90368',
+        fontWeight: '700',
+        alignSelf: 'center'
     }
 });
 
